afc771671e1ea7ee25954cc0f8aecfb97af07c57
Chuks Alimele <chuksalimele@gmail.com>
2024-10-26 20:43:48 +0100

    more coding on OrderIDUtil tesing

--- test/order/OrderIDTest.java
+++ test/order/OrderIDTest.java
@@ -20,16 +20,17 @@ public class OrderIDTest {
     public static void main(String[] args) throws SQLException {
         List<String> market_ids = new LinkedList();
         List<String> pending_ids = new LinkedList();
-
+        
+        int COUNT = 3;
 
         //create markets order ids  the relatated ids
         int acc = 1000;
         int identifier = 222222;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < COUNT; i++) {
             acc++;
             String mrk_id = OrderIDUtil.createMarketOrderID(acc, ""+(++identifier));
             market_ids.add(mrk_id);
-            for (int k = 0; k < 10; k++) {                
+            for (int k = 0; k < COUNT; k++) {                
                 
                 String stp_loss = OrderIDUtil.createModifyStoplossOrderID(mrk_id, ""+(++identifier));
                 String trg_loss = OrderIDUtil.createModifyTargetOrderID(mrk_id, ""+(++identifier));
@@ -41,11 +42,11 @@ public class OrderIDTest {
         }
         
         //create pending order ids  the relatated ids
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < COUNT; i++) {
             acc++;
             String pend_id = OrderIDUtil.createPendingOrderID(acc, ""+(++identifier));
             pending_ids.add(pend_id);
-            for (int k = 0; k < 10; k++) {                
+            for (int k = 0; k < COUNT; k++) {                
                 
                 String stp_loss = OrderIDUtil.createModifyStoplossOrderID(pend_id, ""+(++identifier));
                 String trg_loss = OrderIDUtil.createModifyTargetOrderID(pend_id, ""+(++identifier));
@@ -58,18 +59,59 @@ public class OrderIDTest {
             }
         }        
         
+        System.out.println("==========================================================");        
+        System.out.println("==================MARKET ORDER RELATED IDS===========================");
+        System.out.println("==========================================================");        
         List<MarketOrderIDFamily> mrkFamLst = OrderIDUtil.groupByMarketOrderIDFamily(market_ids);
         
         for(int i=0; i< mrkFamLst.size(); i++){
             MarketOrderIDFamily family = mrkFamLst.get(i);
             
+            System.out.println(family.getMarketOrderID());
+            
+            for(int k=0; k<  family.getModifyStoplossOrderIDs().size(); k++){
+                System.out.println(family.getModifyStoplossOrderIDs().get(k));
+            }
+            for(int k=0; k<  family.getModifyTargetOrderIDs().size(); k++){
+                System.out.println(family.getModifyTargetOrderIDs().get(k));
+            }
+            for(int k=0; k<  family.getCloseOrderIDs().size(); k++){
+                System.out.println(family.getCloseOrderIDs().get(k));
+            }
+            
+            System.out.println("----------------------------------------------");            
+            System.out.println();            
+            System.out.println("----------------------------------------------");            
+            
         }
+        System.out.println("==========================================================");                
+        System.out.println("==================PENDING ORDER RELATED IDS===========================");
+        System.out.println("==========================================================");
         
-        List<PendingOrderIDFamily> pndFamLst = OrderIDUtil.groupByPendingOrderIDFamily(pending_ids);
+        List<PendingOrderIDFamily> pndFamLst = OrderIDUtil.groupByPendingOrderIDFamily(pending_ids);                
         
         for(int i=0; i< pndFamLst.size(); i++){
             PendingOrderIDFamily family = pndFamLst.get(i);
             
+            System.out.println(family.getPendingOrderID());
+                        
+            for(int k=0; k<  family.getModifyStoplossOrderIDs().size(); k++){
+                System.out.println(family.getModifyStoplossOrderIDs().get(k));
+            }
+            for(int k=0; k<  family.getModifyTargetOrderIDs().size(); k++){
+                System.out.println(family.getModifyTargetOrderIDs().get(k));
+            }
+            for(int k=0; k<  family.getModifyEntryPriceOrderIDs().size(); k++){
+                System.out.println(family.getModifyEntryPriceOrderIDs().get(k));
+            }
+
+            for(int k=0; k<  family.getDeleteOrderIDs().size(); k++){
+                System.out.println(family.getDeleteOrderIDs().get(k));
+            }            
+            
+            System.out.println("----------------------------------------------");            
+            System.out.println();            
+            System.out.println("----------------------------------------------");                        
         }
     }
 }
